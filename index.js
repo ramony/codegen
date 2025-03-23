34cac937c11a005b26404734d1f5f0aa037d0716
Ray <Ray@test.com>
2025-03-23 14:36:20 +0800

    update

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql2');
+
+const { parse, render } = require('velocityjs');
 const fs = require('fs');
+const path = require('path');
 
 // 从 JSON 文件读取 MySQL 配置
-const readConfig = () => {
+const readJsonFile = (jsonFile) => {
   try {
-    const data = fs.readFileSync('database.json', 'utf8');
+    const data = fs.readFileSync(jsonFile, 'utf8');
     return JSON.parse(data);
   } catch (err) {
     console.error('读取配置文件失败:', err);
@@ -39,14 +42,14 @@ const getTables = (connection) => {
 };
 
 // 获取指定表的字段结构
-const getColumns = (connection, table) => {
+const getFields = (connection, table) => {
   return new Promise((resolve, reject) => {
     connection.query(`DESCRIBE ${table}`, (err, results) => {
       if (err) {
         console.error(`获取表 ${table} 的字段结构失败:`);
         resolve(null);
       } else {
-        const columns = results.map(row => ({
+        const fields = results.map(row => ({
           field: row.Field,
           type: row.Type,
           isnull: row.Null,
@@ -54,41 +57,137 @@ const getColumns = (connection, table) => {
           def: row.Default,
           extra: row.Extra
         }));
-        resolve(columns);
+        resolve(fields);
       }
     });
   });
 };
 
-const main = async () => {
-  const tableNames = process.argv.slice(2);
-  if (tableNames.length == 0) {
-    console.log(`需要指定表名`);
-    return;
-  }
-  const config = readConfig();
-  const connection = connectToDatabase(config);
-
+const loadTables = async (connection, tableNames) => {
   const tables = [];
   try {
-
     for (const tableName of tableNames) {
-      const table = { name: tableName, columns: [] }
+      const table = { name: tableName, fields: [] }
       //console.log(`表名: ${tableName}`);
-      const columns = await getColumns(connection, tableName);
-      columns?.forEach(column => {
-        table.columns.push(column)
-        //console.log(`  字段名: ${column.Field}, 类型: ${column.Type}, 是否可为空: ${column.Null}, 键类型: ${column.Key}, 默认值: ${column.Default}, 额外信息: ${column.Extra}`);
+      const fields = await getFields(connection, tableName);
+      fields?.forEach(field => {
+        table.fields.push(field)
       });
       tables.push(table)
-      //console.log('-' * 50);
     }
-    console.log(JSON.stringify(tables, 4, 1))
+    return tables
   } catch (err) {
     console.error('发生错误:');
   } finally {
     connection.end();
   }
+}
+
+const main = async () => {
+  const args = process.argv.slice(2);
+  if (args.length == 0) {
+    console.log(`需要task配置文件`);
+    return;
+  }
+  const config = readJsonFile(args[0]);
+  const connection = connectToDatabase(config.database);
+  const tables = await loadTables(connection, config.tableNames);
+
+  const vmFolder = "templates\\" + config.template;
+  const vmFiles = listVM(vmFolder);
+
+  const globalCotent = readVM(vmFolder + "\\" + "global.vm")
+  const templates = []
+  for (const fileName of vmFiles) {
+    const vmContent = globalCotent + readVM(vmFolder + "\\" + fileName)
+    templates.push(vmContent)
+  }
+
+  const typeMapping = readJsonFile("typeMapping.json")
+
+  const contexts = dbToJava(tables, typeMapping)
+
+  for (const context of contexts) {
+    console.log(JSON.stringify(context))
+    for (const template of templates) {
+      const output = render(template, context);
+      console.log(output)
+    }
+  }
+
 };
+
+/**
+ * field: row.Field,
+          type: row.Type,
+          isnull: row.Null,
+          key: row.Key,
+          def: row.Default,
+          extra: row.Extra
+ * @param {*} tables 
+ * @returns 
+ */
+function dbToJava(tables, typeMapping) {
+  return tables.map(it => {
+    const TableName = convertToCamelCase(it.name.replace(/^t_/, ""));
+    const tableName = lowerCaseFirstLetter(TableName);
+    const tableFields = it.fields.map(col => {
+      const Field = convertToCamelCase(col.field);
+      const field = lowerCaseFirstLetter(Field);
+
+      const fieldType = col.type.replace(" unsigned", "");
+      let type = typeMapping.filter(t => {
+        if (t.matchType == 'REGEX') {
+          return new RegExp(t.columnType).test(fieldType);
+        }
+        return false;
+      }).map(t => t.javaType)?.[0];
+      if (!type) {
+        type = "java.lang.Object";
+      }
+
+      return { ...col, Field, field, type }
+    })
+    return { TableName, tableName, tableFields }
+  })
+}
+
+
+//abc_def 转换成 AbcDef
+function convertToCamelCase(str) {
+  return str.split('_')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join('');
+}
+
+// AbcDef 转换成 abcDef
+function lowerCaseFirstLetter(str) {
+  return str.charAt(0).toLowerCase() + str.slice(1);
+}
+
+// 读取模板文件
+function readVM(fileName) {
+  const vmPath = path.join(__dirname, fileName);
+  const content = fs.readFileSync(vmPath, 'utf8');
+  return content;
+}
+
+function listVM(dir) {
+  try {
+    // 读取指定目录下的所有文件和文件夹
+    const files = fs.readdirSync(dir);
+    return files.filter((file) => {
+      // 检查文件扩展名是否为 .vm
+      if (path.extname(file) === '.vm' && file != "global.vm") {
+        // 打印 .vm 文件的完整路径
+        return true;
+      }
+      return false;
+    });
+  } catch (err) {
+    console.error('读取目录时出现错误:', err);
+  }
+}
+
 
 main();
