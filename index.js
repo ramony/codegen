36d3d25179ea8d62e6bd3961f2edcec897ee2e8c
ramony <ramony@qq.com>
2025-04-12 07:56:49 +0800

    update

--- index.js
+++ index.js
@@ -1,95 +1,16 @@
-const path = require('path');
-const mysql = require('mysql2');
-const { render } = require('velocityjs');
+import path from 'path';
+import Mysql from './lib/mysql.js';
+import velocityjs from 'velocityjs';
 
-const { convertToCamelCase, lowerCaseFirstLetter, sortCode } = require("./lib/strings");
-const { readFile, readJsonFile, writeFile, listFile } = require("./lib/files");
+import { convertToCamelCase, lowerCaseFirstLetter, formatCode } from "./lib/strings.js";
+import { readFile, readJsonFile, writeFile, listFile } from "./lib/files.js";
 
-// 连接到 MySQL 数据库
-const connectToDatabase = (config) => {
-  return mysql.createConnection({
-    host: config.host,
-    port: config.port,
-    user: config.user,
-    password: config.password,
-    database: config.database,
-  });
-};
-
-const getTableComment = async (connection, table) => {
-  return new Promise((resolve) => {
-    connection.query(`SELECT * 
-          FROM INFORMATION_SCHEMA.TABLES 
-          WHERE TABLE_NAME = '${table}'`, (err, results) => {
-      if (err) {
-        console.error(`获取表 ${table} 的comment失败:${err}`);
-        resolve(null);
-      } else {
-        const comments = results.map(row => row.TABLE_COMMENT);
-        resolve(comments[0]);
-      }
-    });
-  });
-}
-
-const getTableFieldsComment = async (connection, table) => {
-  return new Promise((resolve) => {
-    connection.query(`SELECT COLUMN_NAME, COLUMN_COMMENT 
-            FROM INFORMATION_SCHEMA.COLUMNS 
-            WHERE  TABLE_NAME = '${table}'`, (err, results) => {
-      if (err) {
-        console.error(`获取表 ${table} 的comment失败:${err}`);
-        resolve(null);
-      } else {
-        const comments = results.map(row => [row.COLUMN_NAME, row.COLUMN_COMMENT]);
-        resolve(Object.fromEntries(comments));
-      }
-    });
-  });
-}
-
-// 获取指定表的字段结构
-const getFields = (connection, table) => {
-  return new Promise((resolve) => {
-    connection.query(`DESCRIBE ${table}`, (err, results) => {
-      if (err) {
-        console.error(`获取表 ${table} 的字段结构失败:`);
-        resolve(null);
-      } else {
-        const fields = results.map(row => ({
-          field: row.Field,
-          type: row.Type,
-          isnull: row.Null,
-          key: row.Key,
-          def: row.Default,
-          extra: row.Extra
-        }));
-        resolve(fields);
-      }
-    });
-  });
-};
-
-const loadTables = async (connection, tableNames) => {
-  const tables = [];
-  try {
-    for (const tableName of tableNames) {
-      const table = { name: tableName, fields: [] }
-      table.comment = await getTableComment(connection, tableName);
-      const fieldsCommentMap = await getTableFieldsComment(connection, tableName);
-
-      table.fields = await getFields(connection, tableName);
-      table.fields?.forEach(field => {
-        field.comment = fieldsCommentMap[field.field];
-      });
-      tables.push(table);
-    }
-    return tables
-  } catch (err) {
-    console.error('发生错误:', err);
-  } finally {
-    connection.end();
+const loadTables = async (db, tableNames) => {
+  const tablesInfo = [];
+  for (const tableName of tableNames) {
+    tablesInfo.push(await db.querySchema(tableName));
   }
+  return tablesInfo
 }
 
 const main = async () => {
@@ -98,25 +19,28 @@ const main = async () => {
     console.error(`需要task配置文件`);
     return;
   }
-  const config = readJsonFile(args[0]);
-  const connection = connectToDatabase(config.database);
-  const tables = await loadTables(connection, config.tableNames);
 
-  const vmFolder = path.join(__dirname, "templates", config.template);
+  const config = readJsonFile(args[0]);
+  const db = new Mysql(config.database);
+  await db.connect();
+  const tablesInfo = await loadTables(db, config.tableNames);
+  await db.close();
+
+  const vmFolder = path.join("templates", config.template);
   const vmFiles = listFile(vmFolder).filter((file) => path.extname(file) === '.vm' && file != "global.vm");
 
   const globalCotent = readFile(path.join(vmFolder, "global.vm"))
   const templates = []
   for (const vmFile of vmFiles) {
-    const vmContent = globalCotent + "\n\n" + readFile(path.join(vmFolder, vmFile))
+    let vmContent = readFile(path.join(vmFolder, vmFile));
+    vmContent = globalCotent + "\n\n" + vmContent;
     templates.push({ vmFile, vmContent })
   }
 
-  const typeMapping = readJsonFile(path.join(__dirname, "./config/typeMapping.json"))
+  const typeMapping = readJsonFile(path.join("./config/typeMapping.json"))
 
-  const contexts = dbToJava(tables, typeMapping, config.preSet)
-
-  for (const context of contexts) {
+  for (const tableInfo of tablesInfo) {
+    const context = dbToJava(tableInfo, typeMapping, config.preSet);
     console.log(`\n开始处理表:${context.name}`)
     for (const template of templates) {
       context.save = (fileName) => {
@@ -125,63 +49,69 @@ const main = async () => {
       context.package = (pkg) => {
         return "package " + config.prefixPackage + "." + pkg + ";";
       }
-      let output = render(template.vmContent, context);
+      let output = velocityjs.render(template.vmContent, context);
       if (!context.dist) {
         console.log(`模板${template.vmFile}输出文件没指定`);
         continue;
       }
       if (context.dist.includes("java")) {
-        output = sortCode(output)
+        output = formatCode(output)
       }
 
       console.log(`模板${template.vmFile}开始处理`);
-      writeFile(path.join(config.dist, context.dist), output);
+      writeFile(path.join(config.dist, config.prefixPackage.replace(/\./g, '/'), context.dist), output);
     }
   }
 
 };
 
-function dbToJava(tables, typeMapping, preSet) {
-  return tables.map(it => {
-    const nameShort = it.name.replace(/^t_/, "");
-    const TableName = convertToCamelCase(nameShort);
-    const tableName = lowerCaseFirstLetter(TableName);
-    const tablepath = convertToCamelCase(nameShort.split("_").slice(0, 2).join("_")).toLowerCase();
-    const tableComment = it.comment;
-    const tableFields = it.fields.map(col => {
-      const Field = convertToCamelCase(col.field);
-      const field = lowerCaseFirstLetter(Field);
-      const fieldType = col.type.replace(" unsigned", "");
-      const fieldComment = col.comment;
-      let fullType = typeMapping.filter(t => {
-        if (t.matchType == 'REGEX') {
-          return new RegExp(t.columnType).test(fieldType);
-        } else if (t.matchType == 'ORDINARY') {
-          return t.columnType == fieldType;
-        }
-        return false;
-      }).map(t => t.javaType)?.[0];
-      if (!fullType) {
-        fullType = "java.lang.Object";
-      }
-      const type = fullType.split(".").pop()
-      return { ...col, Field, field, fullType, type, fieldComment }
-    })
+const defaultContext = {
+  filter(source, excludedArray) {
+    return source.filter(it => !excludedArray.includes(it.field))
+  },
+  importTypes(fields) {
+    const fieldsType = fields.map(it => it.fullType).filter(it => !it.includes("java.lang"));
+    return [...new Set(fieldsType)].map(it => `import ${it};`).join("\n");
+  }
+}
 
-    const filter = (source, excludedArray) => {
-      return source.filter(it => !excludedArray.includes(it.field))
+function dbToJava(tableInfo, typeMapping, preSet) {
+  const { name } = tableInfo;
+  const nameShort = name.replace(/^t_/, "");
+  const Table = convertToCamelCase(nameShort);
+  const table = lowerCaseFirstLetter(Table);
+  const tablepath = convertToCamelCase(nameShort.split("_").slice(0, 2).join("_")).toLowerCase();
+  const tableComment = tableInfo.comment;
+  const tableFields = tableInfo.fields.map(col => {
+    const Field = convertToCamelCase(col.field);
+    const setField = 'set' + Field;
+    const getField = 'get' + Field;
+
+    const field = lowerCaseFirstLetter(Field);
+    const fieldType = col.type.replace(" unsigned", "");
+    const fieldComment = col.comment;
+    let fullType = typeMapping.filter(t => {
+      if (t.matchType == 'REGEX') {
+        return new RegExp(t.columnType).test(fieldType);
+      } else if (t.matchType == 'ORDINARY') {
+        return t.columnType == fieldType;
+      }
+      return false;
+    }).map(t => t.javaType)?.[0];
+    if (!fullType) {
+      fullType = "java.lang.Object";
     }
-    const importTypes = (fields) => {
-      const fieldsType = fields.map(it => it.fullType).filter(it => !it.includes("java.lang"));
-      return [...new Set(fieldsType)].map(it => `import ${it};`).join("\n");
-    }
-    const result = { name: it.name, TableName, tableName, tableComment, tablepath, tableFields, filter, importTypes };
-    for (const pre of preSet) {
-      result["table" + pre] = tableName + pre;
-      result["Table" + pre] = TableName + pre;
-    }
-    return result;
+    const type = fullType.split(".").pop()
+    return { ...col, Field, field, fullType, type, fieldComment, setField, getField }
   })
+
+  const result = { ...defaultContext, name, Table, table, tableComment, tablepath, tableFields };
+  for (const pre of preSet) {
+    result["table" + pre] = table + pre;
+    result["Table" + pre] = Table + pre;
+  }
+  return result;
+
 }
 
 main();
